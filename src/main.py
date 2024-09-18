beb3693b6efd65ed606bbcc1ae3c6ec236051468
Lit Soup <yaelfs09@gmail.com>
2024-09-18 14:27:34 -0400

    init commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       yaelf                                                        #
+# 	Created:      9/18/2024, 2:12:15 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
